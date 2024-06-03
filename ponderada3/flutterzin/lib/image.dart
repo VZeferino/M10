ce0a07463572446a9c9a1e26f588de8b33f888e1
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2024-06-03 12:01:08 -0300

    notification

--- ponderada3/flutterzin/lib/image.dart
+++ ponderada3/flutterzin/lib/image.dart
@@ -3,6 +3,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:typed_data';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class ImageUploadScreen extends StatefulWidget {
   @override
@@ -12,8 +13,49 @@ class ImageUploadScreen extends StatefulWidget {
 class _ImageUploadScreenState extends State<ImageUploadScreen> {
   File? _image;
   Uint8List? _receivedImageData;
-
   final picker = ImagePicker();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    _createNotificationChannel();
+  }
+
+  void _createNotificationChannel() async {
+    var androidNotificationChannel = AndroidNotificationChannel(
+      'image_upload_channel', // id
+      'Image Upload Notifications', // name
+      description: 'Notification channel for image upload status', // description
+      importance: Importance.high,
+    );
+
+    var androidFlutterLocalNotificationsPlugin = flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>();
+    await androidFlutterLocalNotificationsPlugin?.createNotificationChannel(androidNotificationChannel);
+  }
+
+  Future<void> showNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'image_upload_channel', // id
+      'Image Upload Notifications', // name
+      channelDescription: 'Notification channel for image upload status', // description
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+    var platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Upload Concluído',
+      'Sua imagem foi carregada com sucesso!',
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
 
   Future getImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -33,12 +75,13 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
       print('Uploading image: ${image.path}');
       final request = http.MultipartRequest(
         'POST',
-        Uri.parse('http://10.128.0.78:5001/process'), 
+        Uri.parse('http://10.128.0.78:5001/process'),
       );
       request.files.add(await http.MultipartFile.fromPath('image', image.path));
       final response = await request.send();
 
       if (response.statusCode == 200) {
+        await showNotification();
         final responseData = await response.stream.toBytes();
         setState(() {
           _receivedImageData = responseData;
@@ -59,7 +102,7 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
         title: const Text('Processamento de imagens'),
       ),
       body: SingleChildScrollView(
-        child:Center(
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
