e9ca7ed424fa26c797f29c601d0e06a3a9ec8432
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2024-06-14 10:47:08 -0300

    prova2

---
+++ prova2/README.md
@@ -0,0 +1,39 @@
+# Prova prática
+
+Uma API RESTful simples para gerenciamento de blog, construída com FASTAPI. Permite criar, listar, atualizar e deletar posts no blog.
+
+## Começando
+
+Estas instruções irão te ajudar a configurar uma cópia do projeto em execução na sua máquina local para fins de desenvolvimento e teste.
+
+### Pré-requisitos
+
+Antes de iniciar, você precisará ter instalado em sua máquina as seguintes ferramentas:
+- [Docker](https://www.docker.com/get-started)
+- [Docker Compose](https://docs.docker.com/compose/install/)
+
+Isso é necessário para criar o ambiente de contêineres e rodar a aplicação utilizando o Docker Compose.
+
+### Executando a aplicação
+
+Para iniciar o container, execute:
+
+```
+docker compose up 
+```
+
+A aplicação estará acessível em `http://localhost:8000`
+
+
+### Teste de que a roda está funcionando
+
+##### FASTAPI
+![image](https://github.com/VZeferino/M10/assets/99190423/fd677da6-b110-43bc-aa41-13a84af24a65)
+
+
+### Sistema de logs
+
+
+### Testes de rota
+
+No projeto também há o arquivo collection.json para importar as requisições que eu fiz no thunderclient e testá-las.
---
+++ prova2/app.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+import logging
+
+app = FastAPI()
+
+logging.basicConfig(filename='informations.log', level=logging.WARNING, format='%(asctime)s:%(levelname)s:%(message)s')
+
+blog_posts = []
+
+class BlogPost(BaseModel):
+    id: int
+    title: str
+    content: str
+
+@app.post('/blog', status_code=201)
+def create_blog_post(post: BlogPost):
+    blog_posts.append(post)
+    logging.info(f'New blog post created with id {post.id}')
+    return {'status': 'success'}
+
+@app.get('/blog', response_model=List[BlogPost])
+def get_blog_posts():
+    logging.info('Retrieving all blog posts')
+    return blog_posts
+
+@app.get('/blog/{id}', response_model=BlogPost)
+def get_blog_post(id: int):
+    for post in blog_posts:
+        if post.id == id:
+            logging.info(f'Retrieving blog post with id {id}')
+            return post
+    logging.error(f'Blog post with id {id} not found')
+    raise HTTPException(status_code=404, detail='Post not found')
+    
+
+@app.delete('/blog/{id}', status_code=200)
+def delete_blog_post(id: int):
+    for post in blog_posts:
+        if post.id == id:
+            blog_posts.remove(post)
+            logging.warning(f'Blog post with id {id} deleted')
+            return {'status': 'success'}
+    logging.error(f'Blog post with id {id} not found')
+    raise HTTPException(status_code=404, detail='Post not found')
+
+@app.put('/blog/{id}', status_code=200)
+def update_blog_post(id: int, updated_post: BlogPost):
+    for post in blog_posts:
+        if post.id == id:
+            post.title = updated_post.title
+            post.content = updated_post.content
+            logging.info(f'Blog post with id {id} updated')
+            return {'status': 'success'}
+    logging.error(f'Blog post with id {id} not found')
+    raise HTTPException(status_code=404, detail='Post not found')
